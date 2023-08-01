6261e4550f096284b65e24ddbd36547983d6a7a1
Yusei Mizutani <yusei4520030401@gmail.com>
2023-08-01 13:14:31 +0900

    add: dbフォルダの定義

---
+++ backend/server/db/node_db.py
@@ -0,0 +1,53 @@
+import datetime as t
+from typing import List
+from model.node import Node
+from settings import get_db_session
+
+
+def get_all_nodes() -> Node | None:
+    """指定したノードを取得する
+
+    Args:
+        node_id (str): ノードID
+
+    Returns:
+        Node: ノード
+    """
+
+    session = get_db_session()
+    # ノードIDを元にchild_node_id_list、parent_node_id_listに検索をかけ
+    # child_nodesとparent_nodesに検索して得た値を代入する
+
+    session.close()
+    return node
+
+
+def get_nodes(node_ids: list) -> Node | None:
+    """指定したノードを取得する
+
+    Args:
+        node_id (str): ノードID
+
+    Returns:
+        Node: ノード
+    """
+
+    session = get_db_session()
+    # ノードIDを元にchild_node_id_list、parent_node_id_listに検索をかけ
+    # child_nodesとparent_nodesに検索して得た値を代入する
+
+    session.close()
+    return node
+
+
+def add_node(node: Node):
+    """ノードを追加する
+
+    Args:
+        uid (str): ユーザーID
+        spending_amount (int): 支出額
+    """
+    session = get_db_session()
+
+    session.commit()
+    session.close()
--- backend/server/test/test_data.py
+++
@@ -1,4 +0,0 @@
-from model.article import Article
-from model.node import Node
-from model.child_node_id_list import ChildNodeIdList
-from model.parent_node_id_list import ParentNodeIdList
