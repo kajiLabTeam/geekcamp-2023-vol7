e96a80713076f3108add43f914a2b09473669b76
Yusei Mizutani <yusei4520030401@gmail.com>
2023-08-01 13:12:41 +0900

    add: ルータフォルダの追加

---
+++ backend/server/router/article.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from db.article_db import get_article
+
+router = APIRouter()
+
+
+@router.get("/search/article/{nodeId}")
+async def get_article(node_id: int):
+    # db から記事情報を取得する
+    # article = get_article(node_id)
+    # return {"article": article}
+
+    return {"message": "Hello Article"}
