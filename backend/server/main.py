e4cc5aa488b35786e49462df44a92a3c01efb9bc
Yusei Mizutani <yusei4520030401@gmail.com>
2023-08-01 13:13:15 +0900

    add: サーバーを建てるためのメインファイルを作成

--- backend/server/main.py
+++ backend/server/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from router.article import router as article_router
+from router.node import router as node_router
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"greeting": "Hello world"}
+app.include_router(article_router, prefix="/api", tags=["article"])
+app.include_router(node_router, prefix="/api", tags=["node"])
