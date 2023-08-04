d327e36e99a8ce4364578c101ebf522ef26b3a81
Yusei Mizutani <yusei4520030401@gmail.com>
2023-08-04 20:08:33 +0900

    add: DBが立ち上がっていない時、再度DBに繋ぎ直す関数の追加

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -1,8 +1,32 @@
 import os
+import time
 
 from dotenv import load_dotenv
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlmodel import Session, create_engine
+
+
+def create_engine_with_retry(url, max_retries=180, retry_interval=5):
+    retries = 0
+    engine = None
+
+    while retries < max_retries:
+        try:
+            engine = create_engine(url, echo=True)
+            break
+        except OperationalError:
+            retries += 1
+            print(
+                f"Failed to connect to the database. Retrying in {retry_interval} seconds..."
+            )
+            time.sleep(retry_interval)
+
+    if engine is None:
+        raise Exception("Failed to connect to the database after maximum retries.")
+
+    return engine
+
 
 load_dotenv()
 
@@ -13,7 +37,7 @@ db_name = os.getenv("MYSQL_DATABASE")
 
 url: str = f"mysql+mysqlconnector://{user}:{password}@{host}/{db_name}"
 
-engine = create_engine(url, echo=True)
+engine = create_engine_with_retry(url)
 
 session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
