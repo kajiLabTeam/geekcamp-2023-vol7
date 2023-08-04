90ffc0212caba2791c75f4ef6d8d435c68690763
SatooRu65536 <ryuto4619@gmail.com>
2023-08-04 15:35:02 +0900

    fix: api叩く時のurlを修正

--- frontend/components/util/api.ts
+++ frontend/components/util/api.ts
@@ -2,12 +2,14 @@ import { NodeObject } from "@/components/util/type";
 
 // apiを叩く
 async function fetchApi<T>(path: string) {
-  return fetch(`https://host/api/${path}`).then((res) => {
+  const url = new URL(path, "http://localhost:3000/api/");
+
+  return fetch(url).then((res) => {
     return res.json() as T;
   });
 }
 
 // ノードの情報を取得する
 export async function fetchNode(nodeId: number): Promise<NodeObject> {
-  return fetchApi<NodeObject>(`node/${nodeId}`);
+  return fetchApi<NodeObject>(`/node/${nodeId}`);
 }
