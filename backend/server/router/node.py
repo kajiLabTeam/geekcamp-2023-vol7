79ada31be7aee8200d85472dac7f98cc09724321
Yusei Mizutani <yusei4520030401@gmail.com>
2023-08-04 15:59:23 +0900

    fix: ルーティング先のパスを修正

--- backend/server/router/node.py
+++ backend/server/router/node.py
@@ -4,15 +4,7 @@ from fastapi import APIRouter, Query
 router = APIRouter()
 
 
-@router.get("/nodes")
-async def get_nodes():
-    nodes = db_get_nodes(1)
-    return {"nodes": nodes}
-
-
-@router.get("/search/nodes")
-async def get_node(node_ids: list = Query(None)):
-    # db からノード情報を取得する
-    # nodes = db_get_node()
-    # return {"nodes": nodes}
-    return {"message": "Hello Nodes"}
+@router.get("/nodes/connect/{nodeId}")
+async def get_nodes(nodeId):
+    nodes = db_get_nodes(nodeId)
+    return nodes
