da00cbdf8c6959f13cd3a1ecfb7ce40e1ddaf574
k21066-Shimizu <k21066kk@aitech.ac.jp>
2023-08-05 18:19:41 +0900

    change: 取得したノードの処理を改善

--- frontend/hooks/useGraphData.ts
+++ frontend/hooks/useGraphData.ts
@@ -1,45 +1,69 @@
-import { ApiConnectResponse, NodeObject } from "@/components/util/type";
-import { GraphData, Node } from "@/foundation/graph/types";
-import { useState } from "react";
+import { ApiConnectResponse } from "@/components/util/type";
+import { GraphData, Link, Node } from "@/foundation/graph/types";
+import { useRef, useState } from "react";
+
+type LinkKey = `${number}=${number}`;
 
 export default function useGraphData() {
   const [graphData, setGraphData] = useState<GraphData>({ nodes: [], links: [] });
+  const nodesMap = useRef(new Map<number, Node>()).current;
+  const linksMap = useRef(new Map<LinkKey, Link>()).current;
 
-  const fetchConnectNode = async (nodeId: number) => {
-    const response = await fetch(`/api/nodes/connect/${nodeId}`);
+  const fetchConnectNode = async (rootId: number) => {
+    const response = await fetch(`/api/nodes/connect/${rootId}`);
     if (!response.ok) throw Error(`${response.status}: ${response.statusText}`);
     const data = await response.json() as ApiConnectResponse;
     if (data == null) throw Error("data is null");
 
-    const nodes = [data.currentNode, ...data.relationNode].map(nodeobject2Node);
-    nodes[0].isOpened = true;
+    for (const node of [data.currentNode, ...data.relationNode]) {
+      const nodeId = node.nodeId;
+      const key = getLinkKey(rootId, nodeId);
 
-    const links = nodes.map(({ id }) => ({
-      source: nodeId,
-      target: id
-    }));
+      if (!nodesMap.has(nodeId)) {
+        nodesMap.set(nodeId, {
+          id: nodeId,
+          name: node.name,
+          articleId: node.articleId,
+          val: 10
+        });
 
-    const linkDataWithLabel = {
-      nodes: [...nodes, ...nodes.map(v => ({ id: -v.id, val: 1 }))],
-      links: [...links, ...nodes.map(v => ({ source: v.id, target: -v.id, isLabel: true }))]
-    };
+        const labelId = -nodeId;
+        const labelLinkKey = getLinkKey(nodeId, labelId);
+        nodesMap.set(labelId, {
+          id: labelId,
+          val: 10
+        });
 
-    setGraphData(v => ({
-      nodes: [...v.nodes, ...linkDataWithLabel.nodes]
-        .filter((v, i, a) => a.findIndex(w => v.id === w.id) === i),
-      links: [...v.links, ...linkDataWithLabel.links]
-        .filter((v, i, a) => a.findIndex(w => v.source === w.source && v.target === w.target) === i)
-    }));
+        linksMap.set(labelLinkKey, {
+          source: nodeId,
+          target: labelId,
+          isLabel: true
+        });
+      }
 
-    return nodes[0];
+      if (!linksMap.has(key)) {
+        linksMap.set(key, {
+            source: rootId,
+            target: node.nodeId
+        });
+      }
+    }
+
+    const currentNode = nodesMap.get(rootId)!;
+    currentNode.isOpened = true;
+
+    setGraphData({
+      nodes: [...nodesMap.values()],
+      links: [...linksMap.values()],
+    });
+
+    return currentNode;
   }
 
   return { graphData, fetchConnectNode };
 }
 
-const nodeobject2Node = (nodeobj: NodeObject): Node => ({
-  id: Number(nodeobj.nodeId),
-  name: nodeobj.name,
-  articleId: nodeobj.articleId,
-  val: 10
-});
+const getLinkKey = (source: number, target: number): LinkKey =>
+  source < target
+    ? `${source}=${target}`
+    : `${target}=${source}`;
