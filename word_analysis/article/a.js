b9b28ce8ca7685eff4b5c0fed774fabdb11d6b14
kanakanho <kahoshibakichi@gmail.com>
2023-08-05 22:15:44 +0900

    add: githubとwikipediaからarticleを作成

---
+++ word_analysis/article/a.js
@@ -0,0 +1,52 @@
+const tmp = [
+  {
+    type: "standard",
+    title: "JavaScript",
+    displaytitle: '<span class="mw-page-title-main">JavaScript</span>',
+    namespace: { id: 0, text: "" },
+    wikibase_item: "Q2005",
+    titles: {
+      canonical: "JavaScript",
+      normalized: "JavaScript",
+      display: '<span class="mw-page-title-main">JavaScript</span>',
+    },
+    pageid: 2028,
+    thumbnail: {
+      source:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/langja-320px-Unofficial_JavaScript_logo_2.svg.png",
+      width: 320,
+      height: 320,
+    },
+    originalimage: {
+      source:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/langja-512px-Unofficial_JavaScript_logo_2.svg.png",
+      width: 512,
+      height: 512,
+    },
+    lang: "ja",
+    dir: "ltr",
+    revision: "96093701",
+    tid: "4d585f50-279d-11ee-b9a0-3fbc4e55ea45",
+    timestamp: "2023-07-21T08:05:02Z",
+    description: "プログラミングに用いるスクリプト言語の一つ",
+    description_source: "central",
+    content_urls: {
+      desktop: {
+        page: "https://ja.wikipedia.org/wiki/JavaScript",
+        revisions: "https://ja.wikipedia.org/wiki/JavaScript?action=history",
+        edit: "https://ja.wikipedia.org/wiki/JavaScript?action=edit",
+        talk: "https://ja.wikipedia.org/wiki/%E3%83%8E%E3%83%BC%E3%83%88:JavaScript",
+      },
+      mobile: {
+        page: "https://ja.m.wikipedia.org/wiki/JavaScript",
+        revisions: "https://ja.m.wikipedia.org/wiki/Special:History/JavaScript",
+        edit: "https://ja.m.wikipedia.org/wiki/JavaScript?action=edit",
+        talk: "https://ja.m.wikipedia.org/wiki/%E3%83%8E%E3%83%BC%E3%83%88:JavaScript",
+      },
+    },
+    extract:
+      "JavaScript（ジャバスクリプト）は、プログラミング言語であり、HyperText Markup Language（HTML）やCascading Style Sheets（CSS）と並ぶ　World Wide Web（WWW）の中核技術の一つである。",
+    extract_html:
+      "<p><b>JavaScript</b>（ジャバスクリプト）は、プログラミング言語であり、HyperText Markup Language（HTML）やCascading Style Sheets（CSS）と並ぶ　World Wide Web（WWW）の中核技術の一つである。</p>",
+  },
+];
--- word_analysis/article/init.py
+++ word_analysis/article/init.py
@@ -4,39 +4,134 @@ import csv
 import pandas as pd
 import wikipedia
 import sqlite3
+import datetime
+from time import sleep
+import sys
+
+DB_PATH = "connection.db"
+# GITHUB_TOKEN = "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"
+GITHUB_TOKEN = "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"
+
+# データベースに接続
+conn = sqlite3.connect(DB_PATH)
+cursor = conn.cursor()
+
+# 今日の日付を取得
+today = datetime.date.today()
+today = str(today)
+today = today.replace("-", ".")
+today = today[2:]
+
+
+def get_summary_from_github(query):
+    headers = {"Authorization": f"Bearer {GITHUB_TOKEN}"}
+    url = f"https://api.github.com/search/topics?per_page=100&q={query}"
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    # queryを小文字に
+    lower_query = query.lower()
+    # #をsharpに
+    lower_query = lower_query.replace("#", "sharp")
+    # +をpに
+    lower_query = lower_query.replace("+", "p")
+
+    # queryを大文字に
+    upper_query = query.upper()
+
+    # "name"とquerryの一致するものの、"description","updated_at"を取得
+    try:
+        for item in data["items"]:
+            if (
+                item["name"] == query
+                or item["name"] == lower_query
+                or item["name"] == upper_query
+            ):
+                last_up = item["updated_at"]
+                last_up = last_up.replace("-", ".")
+                # 最初の7文字を取得
+                last_up = last_up[:10]
+                description = item["description"]
+
+                if description == "NULL":
+                    description = "None"
+                # dbの"tag"の一番下に格納
+                cursor.execute(
+                    "INSERT INTO articles (tag, article, last_up) VALUES (?, ?, ?);",
+                    (query, description, last_up),
+                )
+                conn.commit()
+                return True
+    except:
+        print(data)
+        sys.exit()
+    # 一致するものがなかった場合
+    return False
 
 
 def get_summary_from_wikipedia(query):
+    text = ""
     url = f"https://ja.wikipedia.org/api/rest_v1/page/summary/{query}"
     response = requests.get(url)
     data = response.json()
-    return data.get("extract", "")
+    text = data.get("extract", "")
+    # textが複数行のとき、textの中身を全て消す
+    if "\n" in text:
+        text = ""
+
+    # textが空じゃないとき
+    if text:
+        # dbの"tag"の一番下に格納
+        cursor.execute(
+            "INSERT INTO articles (tag, article, last_up) VALUES (?, ?, ?);",
+            (query, text, today),
+        )
+        conn.commit()
+        return True
+    else:
+        return False
 
 
-# result/jaccard_coef_data_{tag}.csvのデータを全て取得
-with open("data/tags.csv", "r", encoding="utf-8") as file:
-    lines = file.readlines()
-    # linesの各値で"\n"を削除
-    lines = [line.replace("\n", "") for line in lines]
-all_tag = lines
-all_tag = all_tag[266:]
+# connection.dbのtagsテーブルから"tag"の値を全て取得
+cursor.execute("SELECT tag FROM tags WHERE items_count > 15")
+all_tag = cursor.fetchall()
+all_tag = [tag[0] for tag in all_tag]
+
+# articlesテーブルから"id"の最大値を取得
+cursor.execute("SELECT MAX(id) FROM articles")
+max_id = cursor.fetchall()
+max_id = max_id[0][0]
+
+# all_tagから先頭に#のついた要素を削除
+all_tag = [tag for tag in all_tag if tag[0] != "#"]
+
+# all_tagの前10を削除
+counter = max_id - 2
+all_tag = all_tag[counter:]
+
 
 for name in all_tag:
-    print(name)
+    counter += 1
+
+    bool = True
+    # print(name)
     if not name:
         continue
 
-    summary = get_summary_from_wikipedia(name)
+    if not get_summary_from_github(name):
+        if not get_summary_from_wikipedia(name):
+            cursor.execute(
+                "INSERT INTO articles (tag, article, last_up) VALUES (?, ?, ?);",
+                (name, "None", today),
+            )
+            conn.commit()
+            print(f"ccount:{counter},item:{name},prosseing:None")
+        else:
+            print(f"ccount:{counter},item:{name},prosseing:wiki")
+    else:
+        print(f"ccount:{counter},item:{name},prosseing:github")
 
-    if not summary:
-        print(f"Unfind: {name}")
+    sleep(3)
 
-    print(summary)
-    judge = input()
-    if judge:
-        summary = judge
-
-    with open(f"data/wiki/{name}.txt", "w", encoding="utf-8") as file:
-        file.write(summary)
-
-    print(f"storage: {name}")
+# 最後の処理
+conn.close()
