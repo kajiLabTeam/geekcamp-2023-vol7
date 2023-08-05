bc227f70a012deef30e20102938fe295f46d0cf7
SatooRu65536 <ryuto4619@gmail.com>
2023-08-05 21:48:09 +0900

    fix: 検索をAPIと紐付け

--- frontend/components/util/api.ts
+++ frontend/components/util/api.ts
@@ -1,33 +1,37 @@
 import {
+  NodeConnectData,
   NodeObject,
   SearchNodeObject,
   SuggestionObject,
 } from "@/components/util/type";
 
-// apiを叩く
-async function fetchApi<T>(path: string, params?: any) {
-  const url = new URL(path, "http://localhost:3000/api/");
-
-  let fullPath;
-  if (params) fullPath = `${url}?${params.toString()}`;
-  else fullPath = url;
-
-  return fetch(fullPath).then((res) => {
-    return res.json() as T;
-  });
-}
-
 // ノードの情報を取得する
 export async function fetchNode(nodeId: number): Promise<NodeObject> {
-  return fetchApi<NodeObject>(`/node/${nodeId}`);
+  const response = await fetch(`/api/node/${nodeId}`);
+  if (!response.ok) throw Error(`${response.status}: ${response.statusText}`);
+  const data = (await response.json()) as NodeObject;
+
+  return data;
 }
 
 // wordを検索する
 export async function fetchSearchWord(
   word: string
 ): Promise<SearchNodeObject | SuggestionObject> {
-  const params = new URLSearchParams();
-  params.append("query", word);
+  const response = await fetch(`/api/node/search?query=${word}`);
+  if (!response.ok) throw Error(`${response.status}: ${response.statusText}`);
+  const data = (await response.json()) as SearchNodeObject | SuggestionObject;
 
-  return fetchApi<SearchNodeObject | SuggestionObject>("/node/search", params);
+  return data;
+}
+
+// ノードの関係を取得する
+export async function fetchNodeConnect(
+  nodeId: number
+): Promise<NodeConnectData> {
+  const response = await fetch(`/api/nodes/connect/${nodeId}`);
+  if (!response.ok) throw Error(`${response.status}: ${response.statusText}`);
+  const data = (await response.json()) as NodeConnectData;
+
+  return data;
 }
