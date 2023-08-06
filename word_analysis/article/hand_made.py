5baace07efdea2e5ba12738280cb9e035d8c9d10
kanakanho <kahoshibakichi@gmail.com>
2023-08-06 11:07:20 +0900

    feat: スクレイピングの機能追加

--- word_analysis/article/hand_made.py
+++ word_analysis/article/hand_made.py
@@ -1,4 +1,7 @@
 import sqlite3
+import pyperclip
+from bs4 import BeautifulSoup
+import requests
 
 # データベースのパス
 DB_PATH = "connection.db"
@@ -15,7 +18,17 @@ res = cursor.fetchall()
 for i in res:
     query = ""
     print(i[1])
+    pyperclip.copy(i[1])
+    url = f"https://www.google.com/search?q={i[1]}%E3%81%A8%E3%AF"
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, "html.parser")
+    kno_rdesc = soup.find("div", class_="BNeawe s3v9rd AP7Wnd")
+    if kno_rdesc is not None:
+        text = kno_rdesc.get_text()
+        print(text)
     query = input(f"{i[1]}の説明を入力してください。")
+    if not query:
+        query = text
     cursor.execute(
         "UPDATE articles SET article = ? WHERE tag = ?",
         (query, i[1]),
