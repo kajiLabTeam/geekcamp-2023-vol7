7488d0b03abceda1de6d1edfbf7ff3aade29a125
Yusei Mizutani <yusei4520030401@gmail.com>
2023-08-06 15:02:02 +0900

    fix: relationNodeが何もない時エラーを返してしまう不具合を修正

--- backend/server/db/node_db.py
+++ backend/server/db/node_db.py
@@ -14,24 +14,25 @@ def get_nodes(node_id: int):
 
     current_node = Node.get_node_by_id(node_id)
     if current_node is None:
-        return None
+        return []
 
     connections = Connection.get_connection_by_node_id(current_node.id)
-    if connections is None:
-        return None
+    relation_nodes = None
+    if connections is not None:
+        relation_nodes = Node.get_node_by_ids(
+            [connection.connect_node_id for connection in connections]
+        )
 
-    relation_nodes = Node.get_node_by_ids(
-        [connection.connect_node_id for connection in connections]
-    )
-    if relation_nodes is None:
-        return None
+
+    # if relation_nodes is None:
+    #     return None
 
     return {
         "currentNode": {
             "id": current_node.id,
             "name": current_node.node_name,
             "articleId": current_node.article_id,
-            "childNodeNum": len(relation_nodes),
+            "childNodeNum": len(relation_nodes) if relation_nodes is not None else 0,
         },
         "relationNode": [
             {
@@ -42,7 +43,7 @@ def get_nodes(node_id: int):
                     Connection.get_connection_by_node_id(relation_node.id)
                 ),
             }
-            for relation_node in relation_nodes
+            for relation_node in relation_nodes  or []
         ],
     }
 
@@ -59,14 +60,14 @@ def search_node(node_query: str):
         return search_node_partial(node_query)
 
     connections = Connection.get_connection_by_node_id(current_node.id)
-    if connections is None:
-        return None
+    if connections is not None:
+        relation_nodes = Node.get_node_by_ids(
+            [connection.connect_node_id for connection in connections]
+        )
 
-    relation_nodes = Node.get_node_by_ids(
-        [connection.connect_node_id for connection in connections]
-    )
-    if relation_nodes is None:
-        return None
+
+    # if relation_nodes is None:
+    #     return None
 
     return {
         "type": "node",
@@ -74,7 +75,7 @@ def search_node(node_query: str):
             "id": current_node.id,
             "name": current_node.node_name,
             "articleId": current_node.article_id,
-            "childNodeNum": len(relation_nodes),
+            "childNodeNum": len(relation_nodes) if relation_nodes is not None else 0,
         },
         "relationNode": [
             {
@@ -93,7 +94,7 @@ def search_node(node_query: str):
 def search_node_partial(node_query: str):
     suggestions = Node.get_node_by_node_name_partial(node_query)
     if suggestions is None:
-        return None
+        return []
 
     return {
         "type": "suggestion",
