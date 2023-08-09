b532fa5da6bc4414f20b4ac753a08e26bdde23aa
SatooRu65536 <ryuto4619@gmail.com>
2023-08-09 17:19:13 +0900

    add: 編集画面

--- frontend/pages/edit/[nodeId].tsx
+++ frontend/pages/edit/[nodeId].tsx
@@ -1,15 +1,69 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import EditFrame from "@/components/editframe";
 import LoadLogo from "@/components/loadlogo";
-import "easymde/dist/easymde.min.css";
-import dynamic from "next/dynamic";
+import styles from "@/styles/pages/edit.module.scss";
+import markdownit from "markdown-it";
+import { sanitize } from "dompurify";
+import { fetchArticle } from "@/components/util/api";
 
 export default function EditPage() {
-  const Editor = dynamic(
-    () => {
-      const r = import("@/components/editor");
-      return r;
-    },
-    { loading: () => <LoadLogo />, ssr: false }
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [article, setArticle] = useState("");
+  const [markdown, setMarkdown] = useState("");
+  const [nodeName, setNodeName] = useState("");
 
-  return <Editor />;
+  const router = useRouter();
+
+  function markdown2html(md: string) {
+    const mdit = markdownit();
+    const sanitizedHtml = sanitize(mdit.render(md));
+    return sanitizedHtml;
+  }
+
+  useEffect(() => {
+    (async () => {
+      const nodeId = Number(router.query.nodeId);
+      if (Number.isNaN(nodeId)) return;
+
+      const articleSnap = await fetchArticle(nodeId);
+      setArticle(articleSnap.article);
+      setNodeName("node name");
+      setIsLoading(false);
+    })();
+  }, [setIsLoading, setArticle, router.query.nodeId]);
+
+  useEffect(() => {
+    const html = markdown2html(article);
+    setMarkdown(html);
+  }, [article]);
+
+  return (
+    <main className={styles.main}>
+      {isLoading ? (
+        <LoadLogo />
+      ) : (
+        <>
+          <EditFrame />
+          <h1 className={styles.title}>{nodeName}</h1>
+          <div className={styles.edit_container}>
+            <div className={styles.edit_area_container}>
+              <textarea
+                className={styles.edit_area}
+                defaultValue={article}
+                onChange={(e) => setArticle(e.target.value)}
+              ></textarea>
+            </div>
+
+            <div className={styles.preview_container}>
+              <div
+                className={styles.preview_area}
+                dangerouslySetInnerHTML={{ __html: markdown }}
+              ></div>
+            </div>
+          </div>
+        </>
+      )}
+    </main>
+  );
 }
