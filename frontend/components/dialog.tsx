90c3b3a90061fea72233a3b93d5656c7d459322b
SatooRu65536 <ryuto4619@gmail.com>
2023-08-11 15:22:02 +0900

    change: 編集ボタンのデザイン

--- frontend/components/dialog.tsx
+++ frontend/components/dialog.tsx
@@ -134,6 +134,7 @@ export default function Dialog(
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                   onClick={() => router.push(`/edit/${currentNode?.id}`)}
+                  data-tip="編集"
                 >
                   <path
                     fill="currentColor"
