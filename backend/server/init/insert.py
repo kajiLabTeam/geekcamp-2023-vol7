75b8a6944e625746cae0d457e671577f01cde680
Yusei Mizutani <yusei4520030401@gmail.com>
2023-08-12 11:32:05 +0900

    fix: 子ノードが100個返る不具合を修正

--- backend/server/init/insert.py
+++ backend/server/init/insert.py
@@ -15,65 +15,80 @@ def dump_data():
     cursor.execute(f"SELECT node_name, article_id FROM confirm_node;")
     rows = cursor.fetchall()
     for row in rows:
-        output += f'INSERT INTO node (node_name, article_id) VALUES ("{row[0]}", {row[1]});\n'
+        output += (
+            f'INSERT INTO node (node_name, article_id) VALUES ("{row[0]}", {row[1]});\n'
+        )
 
     cursor.execute(
-        f"SELECT node_id, connect_node_id, connection_strength FROM confirm_connection;")
+        f"SELECT node_id, connect_node_id, connection_strength FROM confirm_connection;"
+    )
     rows = cursor.fetchall()
     for row in rows:
-        output += f'INSERT INTO connection (node_id, connect_node_id, connection_strength) VALUES ({row[0]}, {row[1]}, {int(row[2])});\n'
+        output += f"INSERT INTO connection (node_id, connect_node_id, connection_strength) VALUES ({row[0]}, {row[1]}, {int(row[2])});\n"
 
     output_filename = "./init/init.sql"
-    with open(output_filename, 'w') as f:
+    with open(output_filename, "w") as f:
         f.write(output)
 
 
 def format_table():
     conn = sqlite3.connect("./init/data.db")
     cursor = conn.cursor()
-    cursor.execute("""
+    cursor.execute(
+        """
         CREATE TABLE IF NOT EXISTS confirm_article (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             article TEXT,
             last_update TEXT DEFAULT '2023-08-11T09:00:00.00'
         );
-    """)
-    cursor.execute("""
+    """
+    )
+    cursor.execute(
+        """
         CREATE TABLE IF NOT EXISTS confirm_node (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             node_name TEXT,
             article_id INTEGER
         );
-    """)
-    cursor.execute("""
+    """
+    )
+    cursor.execute(
+        """
         CREATE TABLE IF NOT EXISTS confirm_connection (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             node_id INTEGER,
             connect_node_id INTEGER,
             connection_strength REAL
         );
-    """)
+    """
+    )
     cursor.execute("DELETE FROM confirm_node;")
     cursor.execute("DELETE FROM confirm_article;")
     cursor.execute("DELETE FROM confirm_connection;")
 
-    cursor.execute("""
+    cursor.execute(
+        """
         INSERT INTO confirm_article (article) SELECT article FROM article_tmp;
-    """)
+    """
+    )
 
-    cursor.execute("""
+    cursor.execute(
+        """
         INSERT INTO confirm_node (node_name, article_id)
         SELECT at.node_name, ac.id
         FROM confirm_article ac
         JOIN article_tmp at ON ac.article = at.article;
-    """)
-    cursor.execute("""
+    """
+    )
+    cursor.execute(
+        """
         INSERT INTO confirm_connection (node_id, connect_node_id, connection_strength)
         SELECT nc1.id, nc2.id, c.connection_strength
         FROM connection c
         JOIN confirm_node nc1 ON c.node_id = nc1.node_name
         JOIN confirm_node nc2 ON c.connect_node_id = nc2.node_name;
-    """)
+    """
+    )
 
     conn.commit()
 
