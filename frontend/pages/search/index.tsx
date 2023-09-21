3eaa754d5760acb3f9d8ed34840579ae77eab603
SatooRu65536 <ryuto4619@gmail.com>
2023-09-21 15:53:08 +0900

    add: 検索ボックス追加

--- frontend/pages/search/index.tsx
+++ frontend/pages/search/index.tsx
@@ -1,7 +1,11 @@
+import styles from "@/styles/pages/search.module.scss";
+import Frame from "@/components/frame";
 import { useLocalStorage } from "@mantine/hooks";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function Search() {
+  const [searchValue, setSearchValue] = useState("");
   const [_, setIsFirst] = useLocalStorage({
     key: "isFirst",
     defaultValue: true,
@@ -11,5 +15,35 @@ export default function Search() {
     setIsFirst(false);
   }, [setIsFirst]);
 
-  return <div>検索</div>;
+  function search() {
+    console.log(searchValue);
+  }
+
+  return (
+    <main className={styles.search}>
+      <div className={styles.search_box}>
+        <div className={styles.image_wrapper}>
+          <Image
+            className={styles.logo}
+            src={"/images/logo.png"}
+            alt={"logo"}
+            width={"500"}
+            height={"400"}
+            priority={true}
+          />
+        </div>
+        <input
+          type="text"
+          className={styles.search_input}
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") search();
+          }}
+        />
+        <p className={styles.search_title}>始まりのノードを探してみましょう</p>
+      </div>
+      <Frame revColor={true} />
+    </main>
+  );
 }
